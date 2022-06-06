da7348d73519a4068aa4d208b68168ebaf4158d4
Ivam <ihortretyakmaks@gmail.com>
2022-06-06 15:36:27 +0300

    new js

---
+++ js/operator.js
@@ -0,0 +1,18 @@
+'use strict';
+
+console.log('arr' + " - object");
+console.log(4 + "5");
+
+let incr = 19,
+    decr = 10;
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(8 === 8); //строгое сравнение 
+
+//&& и
+//|| или
+// ! отрецание 
